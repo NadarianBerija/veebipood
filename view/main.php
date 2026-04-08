c8e0129f2c16cb5e1188a01a9bc0d8fb0e7709df
NadarianBerija <nadarian.beria@gmail.com>
2026-04-08 14:04:52 +0300

    Added main.php

---
+++ view/main.php
@@ -0,0 +1,147 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>VIHMART</title>
+</head>
+<body>
+    <div class="wrapper">
+        <header>
+            <div>
+                <a href="">
+                    <img src="" alt="logo">
+                </a>
+
+                <nav>
+                    <ul>
+                        <li><a href="">Avaleht</a></li>
+                        <li><a href="">Pood</a></li>
+                        <li><a href="">Meist</a></li>
+                        <li><a href="">Kontakt</a></li>
+                    </ul>
+
+                    <ul>
+                        <li><a href="">MAAL</a></li>
+                        <li><a href="">ILLUSTRATSIOON</a></li>
+                        <li><a href="">PLAKAT</a></li>
+                        <li><a href="">KUJUNDUS</a></li>
+                        <li><a href="">FOTO</a></li>
+                        <li><a href="">RUUM</a></li>
+                    </ul>
+                </nav>
+
+                <div>
+                    <a href="">EESTI</a>
+                </div>
+            </div>
+        </header>
+
+        <section>
+            <div>
+                <h1>Tere!</h1>
+                <a href="">Kirjuta meil</a>
+                <div>
+                    <p>Saate tellida individuaalse töö vastavalt oma soovidele.</p>
+                </div>
+            </div>
+
+            <div>
+                <div>
+                    <a href="">
+                        <div>
+                            <img src="" alt="maal">
+                                <div>
+                                    <p>MAAL</p>
+                                </div>
+                            </img>
+                        </div>
+                    </a>
+                </div>
+                <div>
+                    <a href="">
+                        <div>
+                            <img src="" alt="illustratsioon">
+                                <div>
+                                    <p>ILLUSTRATSIOON</p>
+                                </div>
+                            </img>
+                        </div>
+                    </a>
+                </div>
+                <div>
+                    <a href="">
+                        <div>
+                            <img src="" alt="plakat">
+                                <div>
+                                    <p>PLAKAT</p>
+                                </div>
+                            </img>
+                        </div>
+                    </a>
+                </div>
+                <div>
+                    <a href="">
+                        <div>
+                            <img src="" alt="kujundus">
+                                <div>
+                                    <p>KUJUNDUS</p>
+                                </div>
+                            </img>
+                        </div>
+                    </a>
+                </div>
+                <div>
+                    <a href="">
+                        <div>
+                            <img src="" alt="foto">
+                                <div>
+                                    <p>FOTO</p>
+                                </div>
+                            </img>
+                        </div>
+                    </a>
+                </div>
+                <div>
+                    <a href="">
+                        <div>
+                            <img src="" alt="ruum">
+                                <div>
+                                    <p>RUUM</p>
+                                </div>
+                            </img>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </section>
+
+        <footer>
+            <div>
+                <nav>
+                    <ul>
+                        <li><a href="">Avaleht</a></li>
+                        <li><a href="">Pood</a></li>
+                        <li><a href="">Meist</a></li>
+                        <li><a href="">Kontakt</a></li>
+                    </ul>
+
+                    <ul>
+                        <li><a href="">MAAL</a></li>
+                        <li><a href="">ILLUSTRATSIOON</a></li>
+                        <li><a href="">PLAKAT</a></li>
+                        <li><a href="">KUJUNDUS</a></li>
+                        <li><a href="">FOTO</a></li>
+                        <li><a href="">RUUM</a></li>
+                    </ul>
+                </nav>
+
+                <div>
+                    <a href="">EESTI</a>
+                </div>
+                <p>&copy; Vihmart 2026. Kõik õigused reserves.</p>
+            </div>
+        </footer>
+    </div>
+</body>
+</html>
