9eb5fcf899868daa54c52fb40f5696542bc5e752
NadarianBerija <nadarian.beria@gmail.com>
2026-04-08 14:58:42 +0300

    Added aboutUs.php and contact.php

---
+++ view/aboutUs.php
@@ -0,0 +1,25 @@
+<div>
+    <h2>Meist</h2>
+
+    <div>
+        <div>
+            <img src="../public/images/users/user.jpg" alt="">
+        </div>
+        <h4>Nimi</h4>
+        <p>Tekst</p>
+    </div>
+    <div>
+        <div>
+            <img src="../public/images/users/user.jpg" alt="">
+        </div>
+        <h4>Nimi</h4>
+        <p>Tekst</p>
+    </div>
+    <div>
+        <div>
+            <img src="../public/images/users/user.jpg" alt="">
+        </div>
+        <h4>Nimi</h4>
+        <p>Tekst</p>
+    </div>
+</div>
---
+++ view/contact.php
@@ -0,0 +1,32 @@
+<div>
+    <h2>Kontakt</h2>
+
+    <p>Saate tellida individuaalse töö vastavalt oma soovidele.</p>
+
+    <div>
+        <div>
+            <h4>Nimi</h4>
+            <p>email</p>
+            <div>
+                <img src="../public/images/site/inst_icon.png" alt="inst_icon">
+                <a href="">@instagram</a>
+            </div>
+        </div>
+        <div>
+            <h4>Nimi</h4>
+            <p>email</p>
+            <div>
+                <img src="../public/images/site/inst_icon.png" alt="inst_icon">
+                <a href="">@instagram</a>
+            </div>
+        </div>
+        <div>
+            <h4>Nimi</h4>
+            <p>email</p>
+            <div>
+                <img src="../public/images/site/inst_icon.png" alt="inst_icon">
+                <a href="">@instagram</a>
+            </div>
+        </div>
+    </div>
+</div>
