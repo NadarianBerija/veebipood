0249af4776d3455db6511fcc72ae49065bb99887
NadarianBerija <nadarian.beria@gmail.com>
2026-04-08 15:17:57 +0300

    Added allArtsShop.php

---
+++ view/allArtsShop.php
@@ -0,0 +1,47 @@
+<div>
+    <h2>Pood</h2>
+    <form action="">
+        <select name="" id="">
+            <option value="Kõik"></option>
+            <option value="Maal"></option>
+            <option value="Illustratsioon"></option>
+            <option value="Plakat"></option>
+            <option value="Kujundus"></option>
+            <option value="Foto"></option>
+            <option value="Ruum"></option>
+        </select>
+    </form>
+
+    <div>
+        <a href="">
+            <div>
+                <img src="../public/images/arts/Maal/1/69b173c76687f.jpg" alt="">
+            </div>
+            <div>
+                <h3>Nimi</h3>
+                <p>Tekst</p>
+                <p>Hind</p>
+            </div>
+        </a>
+        <a href="">
+            <div>
+                <img src="../public/images/arts/Maal/1/69b173c76687f.jpg" alt="">
+            </div>
+            <div>
+                <h3>Nimi</h3>
+                <p>Tekst</p>
+                <p>Hind</p>
+            </div>
+        </a>
+        <a href="">
+            <div>
+                <img src="../public/images/arts/Maal/1/69b173c76687f.jpg" alt="">
+            </div>
+            <div>
+                <h3>Nimi</h3>
+                <p>Tekst</p>
+                <p>Hind</p>    
+            </div>
+        </a>
+    </div>
+</div>
