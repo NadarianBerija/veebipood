1c89ddd424dab0748b0de755f87021c5aaa1dfa5
NadarianBerija <nadarian.beria@gmail.com>
2026-04-09 14:17:17 +0300

    Added Database

---
+++ inc/Database.php
@@ -0,0 +1,63 @@
+<?php
+
+class Database {
+    private $conn;
+    private $host;
+    private $user;
+    private $password;
+    private $baseName;
+
+    function __construct() {
+        $this->host = 'localhost';
+        $this->user = 'root';
+        $this->password = '';
+        $this->baseName = 'vihmart';
+        $this->connect();
+    }
+
+    private function __clone() {}
+
+    function __destruct() {
+        $this->disconnect();
+    }
+
+    function connect() {
+        if (!$this->conn) {
+            try {
+                $this->conn = new PDO("mysql:host={$this->host};dbname={$this->baseName}", $this->user, $this->password, [
+                    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+                    PDO::ATTR_EMULATE_PREPARES => false
+                ]);
+            } catch (PDOException $e) {
+                error_log($e->getMessage());
+                die("Database connection error");
+            }
+        }
+        return $this->conn;
+    }
+
+    function disconnect() {
+        if ($this->conn) {
+            $this->conn = null;
+        }
+    }
+
+    function getOne($query, $params = []) {
+        $stmt = $this->conn->prepare($query);
+        $stmt->execute($params);
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+
+    function getAll($query, $params = []) {
+        $stmt = $this->conn->prepare($query);
+        $stmt->execute($params);
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    function executeRun($query, $params = []) {
+        $stmt = $this->conn->prepare($query);
+        return $stmt->execute($params);
+    }
+}
+
