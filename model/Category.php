9e40d311453330e534915921a40ca99fb95e1f1f
NadarianBerija <nadarian.beria@gmail.com>
2026-04-13 15:41:25 +0300

    Added model Category

---
+++ model/Category.php
@@ -0,0 +1,16 @@
+<?php
+class Category {
+    public static function getAllCategory() {
+        $query = "SELECT c.id AS category_id,
+                        c.cat_img AS cat_img,
+                        cl.name AS category_name,
+                        l.code AS language_code
+                    FROM categories c
+                    JOIN cat_lang cl ON cl.cat_id = c.id
+                    JOIN languaes l ON cl.lang_id = l.id";
+        $db = new Database();
+        $arr = $db->getAll($query);
+        return $arr;
+    }
+}
+?>
