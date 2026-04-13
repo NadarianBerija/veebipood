de64d09634b4a6c6770f07eea784716644317d21
NadarianBerija <nadarian.beria@gmail.com>
2026-04-13 14:59:12 +0300

    Added HeroSlider

---
+++ model/HeroSlider.php
@@ -0,0 +1,12 @@
+<?php
+class HeroSlider {
+    public static function getAllHeroSlides() {
+        $query = "SELECT hs.id AS slide_id.
+                        hs.image AS hero_slide
+                    FROM hero_slides hs";
+        $db = new Database();
+        $arr = $db->getAll($query);
+        return $arr;
+    }
+}
+?>
