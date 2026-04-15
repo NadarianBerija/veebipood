85b1217e649ecab48ebc86f57d4c76abb36c32f3
NadarianBerija <nadarian.beria@gmail.com>
2026-04-15 08:47:02 +0300

    Added files with translations of words into Estonian, English and Russian

---
+++ lang/ee/lang.php
@@ -0,0 +1,15 @@
+<?php
+return [
+    // layout
+    'home' => 'Avaleht',
+    'shop' => 'Pood',
+    'about' => 'Meist',
+    'contact' => 'Kontakt',
+    'copyright' => 'Kõik õigused kaitstud',
+    'phrase' => 'Saate tellida individuaalse töö vastavalt oma soovidele.',
+
+    // main
+    'hello' => 'Tere!',
+    'write_us' => 'Kirjuta meil',
+]
+?>
---
+++ lang/en/lang.php
@@ -0,0 +1,15 @@
+<?php
+return [
+    // layout
+    'home' => 'Home',
+    'shop' => 'Shop',
+    'about' => 'About us',
+    'contact' => 'Contact',
+    'copyright' => 'All rights reserved',
+    'phrase' => 'You can order individual work according to your needs.',
+
+    // main
+    'hello' => 'Hello!',
+    'write_us' => 'Write to us',
+]
+?>
