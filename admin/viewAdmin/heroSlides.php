362cdbc05dcc01ba67270062f3483183e2814188
NadarianBerija <nadarian.beria@gmail.com>
2026-04-28 10:40:42 +0300

    Added style to heroSlides

--- admin/viewAdmin/heroSlides.php
+++ admin/viewAdmin/heroSlides.php
@@ -3,17 +3,23 @@ ob_start()
 ?>
 
 <div class="d-flex">
+    <div class="slidesContainer">
     <?php
     if (!empty($arr)) { 
         foreach($arr as $slide) { 
     ?>
-        <img style="width:200px" src="../public/<?= htmlspecialchars($slide['slide_img']) ?>">
+    <div class="card">
+        <div class="slide card-body">
+            <img src="../public/<?= htmlspecialchars($slide['slide_img']) ?>">
+        </div>
+    </div>
     <?php 
         }
     } else {
     echo '<p>Slaidid puuduvad</p>';
     }
     ?>
+    </div>
 </div>
 
 <?php
